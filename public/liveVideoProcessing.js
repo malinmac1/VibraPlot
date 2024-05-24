04c8519491df32a03ed7784529b91fded4131bf6
malinmac1 <malinmac1@gmail.com>
2024-05-24 20:59:40 +0200

    QOL updates

--- public/liveVideoProcessing.js
+++ public/liveVideoProcessing.js
@@ -1,3 +1,4 @@
+//Starting script after OpenCV.js is loaded
 var Module;
 
 Module.onRuntimeInitialized = function () {
@@ -5,61 +6,79 @@ Module.onRuntimeInitialized = function () {
     let streaming = false;
     let start = false;
 
+    // Starting processing after video is loaded
     video.addEventListener('loadeddata', (event) => {
         video.addEventListener('canplay', (event) => {
             streaming = true;
+
+            // Assigning HTML elements to variables
             let video = document.getElementById('videoInput');
             let canvas = document.getElementById('canvasOutput');
             let startButton = document.getElementById('startButton');
-            let x1 = document.getElementById('x1');
-            let x2 = document.getElementById('x2');
-            let y1 = document.getElementById('y1');
-            let y2 = document.getElementById('y2');
-            xValue = document.getElementById('xValue');
-            yValue = document.getElementById('yValue');
+            let left = document.getElementById('left');
+            let width = document.getElementById('width');
+            let bottom = document.getElementById('bottom');
+            let height = document.getElementById('height');
+            let X = document.getElementById('X');
+            let Y = document.getElementById('Y');
+            let frequencySelect = document.getElementById('frequency');
 
+            // Setting up initial x, y values
             canvas.width = video.width;
             canvas.height = video.height;
+            left.value = 0;
+            width.value = 100;
+            bottom.value = 0;
+            height.value = 100;
+            let leftValue = parseInt(left.value) / 100 * canvas.width;
+            let widthValue = parseInt(width.value) / 100 * (canvas.width - leftValue);
+            let bottomValue = canvas.height - (parseInt(bottom.value) / 100 * canvas.height);
+            let heightValue = parseInt(height.value) / 100 * bottomValue;
+            let xValue = leftValue + widthValue/2;
+            let yValue = canvas.height - (bottomValue - heightValue/2);
 
-            x1.value = 0;
-            x2.value = canvas.width;
-            y1.value = canvas.height;
-            y2.value = 0;
+            X.innerHTML = "X: " + xValue;
+            Y.innerHTML = "Y: " + yValue;
 
-            let left = parseInt(x1.value);
-            let right = parseInt(x2.value);
-            let top = canvas.height - parseInt(y1.value);
-            let bottom = canvas.height - parseInt(y2.value);
-            let width = right - left;
-            let height = bottom - top;
-            let X = left + width/2;
-            let Y = canvas.height - (top + height/2);
+            // Setting initial frequency
+            frequencySelect.value = 30;
+            let frequency = parseInt(frequencySelect.value);
 
-            xValue.innerHTML = "X: " + X;
-            yValue.innerHTML = "Y: " + Y;
-
-            x1.onchange = function () {
-                left = parseInt(x1.value);
+            // Changing x, y values when input fields are changed
+            left.onchange = function () {
+                leftValue = parseInt(left.value) / 100 * canvas.width;
+                widthValue = parseInt(width.value) / 100 * (canvas.width - leftValue);
+                xValue = leftValue + widthValue/2;
+                X.innerHTML = "X: " + xValue;
             }
-            x2.onchange = function () {
-                right = parseInt(x2.value);
+            width.onchange = function () {
+                leftValue = parseInt(left.value) / 100 * canvas.width;
+                widthValue = parseInt(width.value) / 100 * (canvas.width - leftValue);
+                xValue = leftValue + widthValue/2;
+                X.innerHTML = "X: " + xValue;
             }
-            y1.onchange = function () {
-                top = canvas.height - parseInt(y1.value);
+            bottom.onchange = function () {
+                bottomValue = canvas.height - (parseInt(bottom.value) / 100 * canvas.height);
+                heightValue = parseInt(height.value) / 100 * bottomValue;
+                yValue = canvas.height - (bottomValue - heightValue/2);
+                Y.innerHTML = "Y: " + yValue;
             }
-            y2.onchange = function () {
-                bottom = canvas.height - parseInt(y2.value);
+            height.onchange = function () {
+                bottomValue = canvas.height - (parseInt(bottom.value) / 100 * canvas.height);
+                heightValue = parseInt(height.value) / 100 * bottomValue;
+                yValue = canvas.height - (bottomValue - heightValue/2);
+                Y.innerHTML = "Y: " + yValue;
             }
 
-            // take first frame of the video
+            // Capturing first frame of the video
             let cap = new cv.VideoCapture(video);
             let frame = new cv.Mat(video.height, video.width, cv.CV_8UC4);
 
-            // hardcode the initial location of window
-            let trackWindow = new cv.Rect(left, top, width, height);
+            // Setting the initial location of track window
+            let trackWindow = new cv.Rect(leftValue, bottomValue - heightValue, widthValue, heightValue);
             cap.read(frame);
 
-            // set up the ROI for tracking
+            // Declaring variables for processing
             let roi;
             let hsvRoi;
             let rgbRoi;
@@ -70,39 +89,39 @@ Module.onRuntimeInitialized = function () {
             let high;
             let roiHist;
             let hsvRoiVec;
-
             let termCrit;
-
             let hsv;
             let dst;
             let hsvVec;
 
-            const FPS = 30;
+            // Changing frequency when input field is changed
+            frequencySelect.onchange = function () {
+                frequency = parseInt(frequencySelect.value);
+                clearInterval(initialLoopInterval);
+                clearInterval(processVideoInterval);
+                initialLoopInterval = setInterval(initialLoop, 1000/frequency);
+            }
 
             let initialLoopInterval;
             let processVideoInterval;
 
+            // Function to show window location without tracking
             function initialLoop() {
                 frame.delete();
                 frame = new cv.Mat(video.height, video.width, cv.CV_8UC4);
                 cap.read(frame);
                 // hardcode the initial location of window
-                trackWindow = new cv.Rect(left, top, width, height);
-                cv.rectangle(frame, new cv.Point(left, top), new cv.Point(right, bottom), [255, 0, 0, 255], 2);
+                trackWindow = new cv.Rect(leftValue, bottomValue - heightValue, widthValue, heightValue);
+                cv.rectangle(frame, new cv.Point(leftValue, bottomValue - heightValue), new cv.Point(leftValue + widthValue, bottomValue), [255, 0, 0, 255], 2);
                 cv.imshow('canvasOutput', frame);
-                width = right - left;
-                height = bottom - top;
-                X = left + width/2;
-                Y = canvas.height - (top + height/2);
-                xValue.innerHTML = "X: " + X;
-                yValue.innerHTML = "Y: " + Y;
             }
 
+            // Function to process video
             function processVideo() {
                 console.log('processVideo function is called.')
                 try {
+                    // Checking if streaming is stopped
                     if (!streaming) {
-                        // clean and stop.
                         frame.delete();
                         dst.delete();
                         hsvVec.delete();
@@ -111,46 +130,48 @@ Module.onRuntimeInitialized = function () {
                         return;
                     }
 
-                    // start processing.
+                    // Starting processing
                     cap.read(frame);
                     cv.cvtColor(frame, hsv, cv.COLOR_RGBA2RGB);
                     cv.cvtColor(hsv, hsv, cv.COLOR_RGB2HSV);
                     cv.calcBackProject(hsvVec, [0], roiHist, dst, [0, 180], 1);
 
-                    // Apply meanshift to get the new location
-                    // and it also returns number of iterations meanShift took to converge,
-                    // which is useless in this demo.
+                    // Tracking using meanshift algorithm
                     [, trackWindow] = cv.meanShift(dst, trackWindow, termCrit);
 
-                    // Draw it on image
+                    // Drawing tracking window
                     let [x, y, w, h] = [trackWindow.x, trackWindow.y, trackWindow.width, trackWindow.height];
                     cv.rectangle(frame, new cv.Point(x, y), new cv.Point(x + w, y + h), [255, 0, 0, 255], 2);
                     cv.imshow('canvasOutput', frame);
-                    x1.value = x;
-                    x2.value = x + w;
-                    y1.value = canvas.height - y;
-                    y2.value = canvas.height - (y + h);
-                    left = x;
-                    right = x + w;
-                    top = y;
-                    bottom = y + h;
-                    X = x + w/2;
-                    Y = canvas.height - (y + h/2);
-                    xValue.innerHTML = "X: " + X;
-                    yValue.innerHTML = "Y: " + Y;
+
+                    // Assigning new values to x, y
+                    left.value = x / canvas.width * 100;
+                    width.value = w / (canvas.width - x) * 100;
+                    bottom.value = (canvas.height - (y + h)) / canvas.height * 100;
+                    height.value = h / (y + h) * 100;
+                    leftValue = x;
+                    bottomValue = y + h;
+                    xValue = x + w/2;
+                    yValue = canvas.height - (y + h/2);
+                    X.innerHTML = "X: " + xValue;
+                    Y.innerHTML = "Y: " + yValue;
                 } catch (err) {
                     console.error(err);
                 }
             }
 
-            initialLoopInterval = setInterval(initialLoop, 1000/FPS);
+            // Starting initial loop
+            initialLoopInterval = setInterval(initialLoop, 1000/frequency);
 
+            // Starting or stopping tracking
             startButton.onclick = function () {
                 if (start === false) {
+                    // Stopping initial loop
                     start = true;
                     startButton.innerHTML = 'Stop';
                     clearInterval(initialLoopInterval);
 
+                    // Processing first frame
                     roi = frame.roi(trackWindow);
                     hsvRoi = new cv.Mat();
                     rgbRoi = new cv.Mat();
@@ -167,7 +188,6 @@ Module.onRuntimeInitialized = function () {
                     hsvRoiVec.push_back(hsvRoi);
                     cv.calcHist(hsvRoiVec, [0], mask, roiHist, [180], [0, 180]);
                     cv.normalize(roiHist, roiHist, 0, 255, cv.NORM_MINMAX);
-
                     roi.delete();
                     rgbRoi.delete();
                     hsvRoi.delete();
@@ -175,20 +195,22 @@ Module.onRuntimeInitialized = function () {
                     low.delete();
                     high.delete();
                     hsvRoiVec.delete();
-
                     termCrit = new cv.TermCriteria(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1);
-
                     hsv = new cv.Mat(video.height, video.width, cv.CV_8UC3);
                     dst = new cv.Mat();
                     hsvVec = new cv.MatVector();
                     hsvVec.push_back(hsv);
 
-                    processVideoInterval = setInterval(processVideo, 1000/FPS);
+                    // Starting processing loop
+                    processVideoInterval = setInterval(processVideo, 1000/frequency);
                 } else {
+                    // Stopping processing loop
                     start = false;
                     startButton.innerHTML = 'Start';
                     clearInterval(processVideoInterval);
-                    initialLoopInterval = setInterval(initialLoop, 1000/FPS);
+
+                    // Starting initial loop
+                    initialLoopInterval = setInterval(initialLoop, 1000/frequency);
                 }
             }
         });
--- public/video.js
+++ public/video.js
@@ -1,16 +1,39 @@
-//Selector for your <video> element
+// Assigning HTML elements to variables
 const video = document.querySelector('#videoInput');
+const videoSelect = document.querySelector('#videoSelect');
 
-//Core
-window.navigator.mediaDevices.getUserMedia({video: true})
-    .then(stream => {
-        video.srcObject = stream;
-        video.onloadedmetadata = (e) => {
-            video.width = visualViewport.width / 5;
-            video.height = video.width * (video.videoHeight / video.videoWidth);
-            video.play();
-        };
-    })
-    .catch(() => {
-        alert('You have to allow camera access to use this feature.');
+// Getting all video devices
+navigator.mediaDevices.enumerateDevices()
+    .then(devices => {
+        const videoDevices = devices.filter(device => device.kind === 'videoinput');
+        videoDevices.forEach(videoDevice => {
+            const option = document.createElement('option');
+            option.value = videoDevice.deviceId;
+            option.text = videoDevice.label || `Camera ${videoSelect.length + 1}`;
+            videoSelect.appendChild(option);
+        });
     });
+
+// Function to get video stream
+function getVideo(deviceId) {
+    navigator.mediaDevices.getUserMedia({ video: { deviceId: deviceId } })
+        .then(stream => {
+            video.srcObject = stream;
+            video.onloadedmetadata = (event) => {
+                video.width = visualViewport.width / 5;
+                video.height = video.width * (video.videoHeight / video.videoWidth);
+                video.play();
+            };
+        })
+        .catch(() => {
+            alert('You have to allow camera access to use this feature.');
+        });
+}
+
+// Initial function call
+getVideo();
+
+// Changing video stream when a new video device is selected
+videoSelect.onchange = () => {
+    getVideo(videoSelect.value);
+};
