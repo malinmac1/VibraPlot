bd1987e9775a773e9574f104d5fca66f77768010
malinmac1 <malinmac1@gmail.com>
2025-06-03 22:37:35 +0200

    Added automatic anonymous login for session management

--- public/sessionCreate.js
+++ public/sessionCreate.js
@@ -5,12 +5,14 @@ onAuthStateChanged(auth, (user) => {
     if (user) {
         // User is signed in
         const uid = user.uid;
+        console.log("User is signed in with UID:", uid);
     } else {
         // User is signed out
         signInAnonymously(auth)
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
+                console.error("Error signing in anonymously:", errorCode, errorMessage);
             });
     }
 });
