7d57142c4f03471520eb4a3d190142f9b1381a9e
malinmac1 <malinmac1@gmail.com>
2025-06-03 23:31:32 +0200

    Added automatic anonymous login for session management

--- public/sessionCreate.js
+++ public/sessionCreate.js
@@ -1,22 +1,27 @@
 import { getAuth, onAuthStateChanged, signInAnonymously } from "firebase/auth";
 
 const auth = getAuth();
+let signedIn = false;
 onAuthStateChanged(auth, (user) => {
     if (user) {
         // User is signed in
         const uid = user.uid;
         console.log("User is already signed in with UID:", uid);
+        signedIn = true;
     } else {
         // User is signed out
-        signInAnonymously(auth)
-            .then(() => {
-                const uid = user.uid;
-                console.log("User anonymously signed in with UID:", uid);
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.error("Error signing in anonymously:", errorCode, errorMessage);
-            });
     }
 });
+if (!signedIn) {
+    console.log("User is not signed in, signing in anonymously...");
+    signInAnonymously(auth)
+        .then(() => {
+            const uid = user.uid;
+            console.log("User anonymously signed in with UID:", uid);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error("Error signing in anonymously:", errorCode, errorMessage);
+        });
+}
