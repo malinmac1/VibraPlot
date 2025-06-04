c82894a77581f512f173027b72e85801ede211c1
malinmac1 <malinmac1@gmail.com>
2025-06-04 13:27:20 +0200

    Added automatic anonymous login for session management

---
+++ public/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDN50CjQks0GbgXz6I3hPOpFcgfGjvlxSc",
+    authDomain: "vibraplot.firebaseapp.com",
+    databaseURL: "https://vibraplot-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "vibraplot",
+    storageBucket: "vibraplot.appspot.com",
+    messagingSenderId: "54044494362",
+    appId: "1:54044494362:web:5286179047790c043fdfa3",
+    measurementId: "G-XY2VRZ77ZV"
+};
+
+// Initializing Firebase
+initializeApp(firebaseConfig);
--- public/sessionCreate.js
+++ public/sessionCreate.js
@@ -2,32 +2,28 @@ import { getAuth, onAuthStateChanged, signInAnonymously } from "https://www.gsta
 
 console.log("Initializing Firebase Auth...");
 const auth = getAuth();
-let signedIn = false;
 onAuthStateChanged(auth, (user) => {
     if (user) {
         // User is signed in
         const uid = user.uid;
         console.log("User is already signed in with UID:", uid);
-        signedIn = true;
     }
-});
-if (!signedIn) {
-    console.log("User is not signed in, signing in anonymously...");
-    signInAnonymously(auth)
-        .then(() => {
-            // Signed in successfully, checking uid again
-            onAuthStateChanged(auth, (user) => {
-                if (user) {
+    else{
+        // Proceeding with anonymous sign-in
+        console.log("User is not signed in, signing in anonymously...");
+        signInAnonymously(auth)
+            .then(() => {
+                // Signed in successfully, checking uid again
+                onAuthStateChanged(auth, (user) => {
                     // User is signed in
                     const uid = user.uid;
                     console.log("User signed in with UID:", uid);
-                    signedIn = true;
-                }
+                });
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.error("Error signing in anonymously:", errorCode, errorMessage);
             });
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.error("Error signing in anonymously:", errorCode, errorMessage);
-        });
-}
+    }
+});
